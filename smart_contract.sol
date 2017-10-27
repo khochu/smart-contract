f1ded007fa53fdb8d6ff31827c98b70e911d9077
AndyHedgeable <andy@hedgeable.com>
2017-10-27 11:43:28 -0400

    Updated variable names

--- smart_contract.sol
+++ smart_contract.sol
@@ -20,14 +20,14 @@ contract owned {
 contract MyToken is owned{
     event Transfer(address indexed from, address indexed to, uint256 value); // Event for a normal transfer of funds
     event Authenticate(address indexed from, address indexed to, uint256 value, bytes data); // Event for when an address is authenticated
-    event Authorize(address target, bool authorize); // Event for when an address is authorized to authenticate
+    event Whitelist(address target, bool whitelist); // Event for when an address is whitelisted to authenticate
 
-    struct authStruct {
+    struct partnerValues {
         uint value;
         string data;
     }
 
-    struct validStruct {
+    struct hedgeableValues {
         uint value;
         string data;
         uint timestamp;
@@ -41,63 +41,72 @@ contract MyToken is owned{
     mapping (address => uint256) public balanceOf;
     /* This creates an array of all whitelisted addresses */
     mapping (address => bool) public whitelist;
-    mapping (address => authStruct) public authStructMap;
-    mapping (address => validStruct) public validStructMap;
+    mapping (address => partnerValues) public partnerValuesMap;
+    mapping (address => hedgeableValues) public hedgeableValuesMap;
 
     /* Initializes contract with initial supply tokens to the creator of the contract */
-    function MyToken(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits, address centralMiner) {
+    function MyToken(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits, address ownerAddr) {
         balanceOf[msg.sender] = initialSupply * uint256(10 ** 18);              // Give the creator all initial tokens
         name = tokenName;                                   // Set the name for display purposes
         symbol = tokenSymbol;                               // Set the symbol for display purposes
         decimals = decimalUnits;                            // Amount of decimals for display purposes
-        if (centralMiner != 0) owner = centralMiner;        // Set the owner of the contract on creation
+        if (ownerAddr != 0) owner = ownerAddr;              // Set the owner of the contract on creation
     }
 
     /* Send coins */
     function transfer(address _to, uint256 _value) {
+        _value = _value * (10 ** 18);
+        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
+        require (balanceOf[_from] > _value);                // Check if the sender has enough
+        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
         _transfer(msg.sender, _to, _value);
         Transfer(msg.sender, _to, _value);
     }
 
     /* Internal transfer, only can be called by this contract */
     function _transfer(address _from, address _to, uint _value) internal {
-        _value = _value * (10 ** 18);
-        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
-        require (balanceOf[_from] > _value);                // Check if the sender has enough
-        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
         balanceOf[_from] -= _value;                         // Subtract from the sender
         balanceOf[_to] += _value;                           // Add the same to the recipient
     }
 
-    function whitelistAddress(address target, bool authorize) onlyOwner {
-        whitelist[target] = authorize;
-        Authorize(target, authorize);
+    /* Function to whitelist partner address. Can only be called by owner */
+    function whitelistAddress(address target, bool whitelistBool) onlyOwner {
+        whitelist[target] = whitelistBool;
+        Whitelist(target, whitelistBool);
     }
 
     /* Function to authenticate user
        Restricted to whitelisted partners */
     function authenticate(address _to, uint256 _value, string data) {
-        require(whitelist[msg.sender]);
+        require(whitelist[msg.sender]);                     // Make sure the sender is whitelisted
+        _value = _value * (10 ** 18);                       // Adjust for decimals
+        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
+        require (balanceOf[_from] > _value);                // Check if the sender has enough
+        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
         _transfer(msg.sender, _to, _value);
-        updateAuthStruct(msg.sender, _value, data);
+        updatePartnerValuesMap(msg.sender, _value, data);
         Authenticate(msg.sender, _to, _value, msg.data);
     }
 
-    function updateAuthStruct(address _sender, uint _value, string data) internal {
-        authStructMap[_sender].value = _value;
-        authStructMap[_sender].data = data;
+    /* Function to update the partnerValuesMap with their amount and challenge string */
+    function updatePartnerValuesMap(address _sender, uint _value, string data) internal {
+        partnerValuesMap[_sender].value = _value;
+        partnerValuesMap[_sender].data = data;
     }
 
-    function updateValidStruct(address _sender, uint _value, string data) onlyOwner {
-        validStructMap[_sender].value = _value;
-        validStructMap[_sender].data = data;
-        validStructMap[_sender].timestamp = block.timestamp + 1 days;
+    /* Function to update the hedgeableValuesMap. Called exclusively from the Hedgeable API */
+    function updateHedgeableValuesMap(address _sender, uint _value, string data) onlyOwner {
+        hedgeableValuesMap[_sender].value = _value;
+        hedgeableValuesMap[_sender].data = data;
+        hedgeableValuesMap[_sender].timestamp = block.timestamp + 1 days;
     }
 
+    /* Function called by Hedgeable API to check if the partner has validated
+       The partners value and data must match and it must be less than a day since the last authentication */
     function validateAuthentication(address _sender) public constant returns (bool _isValid) {
-        if (authStructMap[_sender].value == validStructMap[_sender].value
-        && block.timestamp < validStructMap[_sender].timestamp
-        && sha3(authStructMap[_sender].data) == sha3(validStructMap[_sender].data)){
+        if (partnerValuesMap[_sender].value == hedgeableValuesMap[_sender].value
+        && block.timestamp < hedgeableValuesMap[_sender].timestamp
+        && sha3(partnerValuesMap[_sender].data) == sha3(hedgeableValuesMap[_sender].data)){
             return true;
         }
         return false;
