307d482da0727996bd5e161b16bb4b907ee7d326
AndyHedgeable <andy@hedgeable.com>
2017-11-13 16:43:41 -0500

    Updates to make ERC20 standard

--- smart_contract.sol
+++ smart_contract.sol
@@ -17,56 +17,104 @@ contract owned {
     }
 }
 
-contract MyToken is owned{
-    event Transfer(address indexed from, address indexed to, uint256 value); // Event for a normal transfer of funds
+contract basicToken {
+    function totalSupply() constant returns (uint256 totalSupply) {}
+    function name() constant returns (string name) {}
+    function symbol() constant returns (string symbol) {}
+    function decimals() constant returns (uint8 decimals) {}
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+    function approve(address _spender, uint256 _value) returns (bool success) {}
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract ERC20Standard is basicToken{
+
+    mapping (address => mapping (address => uint256)) allowed;
+    mapping (address => uint256) public balances;
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        require (_to != 0x0);                               // Prevent transfer to 0x0 address
+        require (balances[msg.sender] > _value);            // Check if the sender has enough
+        require (balances[_to] + _value > balances[_to]);   // Check for overflows
+        _transfer(msg.sender, _to, _value);                 // Perform actually transfer
+        Transfer(msg.sender, _to, _value);                  // Trigger Transfer event
+        return true;
+    }
+
+    /* Use admin powers to send from a users account */
+    function transferFrom(address _from, address _to, uint256 _value) {
+        require (_to != 0x0);                               // Prevent transfer to 0x0 address
+        require (balances[msg.sender] > _value);            // Check if the sender has enough
+        require (balances[_to] + _value > balances[_to]);   // Check for overflows
+        require (allowed[_from][msg.sender] >= _value);     // Only allow if sender is allowed to do this
+        _transfer(msg.sender, _to, _value);                 // Perform actually transfer
+        Transfer(msg.sender, _to, _value);                  // Trigger Transfer event
+        return true;
+    }
+
+    /* Internal transfer, only can be called by this contract */
+    function _transfer(address _from, address _to, uint _value) internal {
+        balances[_from] -= _value;                          // Subtract from the sender
+        balances[_to] += _value;                            // Add the same to the recipient
+    }
+
+    /* Get balance of an account */
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    /* Approve an address to have admin power to use transferFrom */
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+}
+
+contract HydroToken is ERC20Standard, owned{
     event Authenticate(address indexed from, address indexed to, uint256 value, bytes data); // Event for when an address is authenticated
-    event Whitelist(address target, bool whitelist); // Event for when an address is whitelisted to authenticate
+    event Whitelist(address target, bool whitelist);                                         // Event for when an address is whitelisted to authenticate
 
     struct partnerValues {
         uint value;
         string data;
     }
 
-    struct hedgeableValues {
+    struct hydrogenValues {
         uint value;
         string data;
         uint timestamp;
     }
 
-    string public name;
-    string public symbol;
-    uint8 public decimals;
+    string public name = "Hydro Token";
+    string public symbol = "H2O";
+    uint8 public decimals = 18;
+    uint256 initialSupply = 10**9;
+    uint256 totalSupply = initialSupply * uint256(10**decimals);
 
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    /* This creates an array of all whitelisted addresses */
+    /* This creates an array of all whitelisted addresses
+     * Must be whitelisted to be able to utilize auth
+     */
     mapping (address => bool) public whitelist;
+
     mapping (address => partnerValues) public partnerValuesMap;
-    mapping (address => hedgeableValues) public hedgeableValuesMap;
+    mapping (address => hydrogenValues) public hydrogenValuesMap;
 
     /* Initializes contract with initial supply tokens to the creator of the contract */
-    function MyToken(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits, address ownerAddr) {
-        balanceOf[msg.sender] = initialSupply * uint256(10 ** 18);              // Give the creator all initial tokens
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
-        decimals = decimalUnits;                            // Amount of decimals for display purposes
-        if (ownerAddr != 0) owner = ownerAddr;              // Set the owner of the contract on creation
-    }
-
-    /* Send coins */
-    function transfer(address _to, uint256 _value) {
-        _value = _value * (10 ** 18);
-        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
-        require (balanceOf[_from] > _value);                // Check if the sender has enough
-        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
-        _transfer(msg.sender, _to, _value);
-        Transfer(msg.sender, _to, _value);
-    }
-
-    /* Internal transfer, only can be called by this contract */
-    function _transfer(address _from, address _to, uint _value) internal {
-        balanceOf[_from] -= _value;                         // Subtract from the sender
-        balanceOf[_to] += _value;                           // Add the same to the recipient
+    function HydroToken(address ownerAddress) {
+        balances[msg.sender] = totalSupply;                 // Give the creator all initial tokens
+        if (ownerAddress != 0) owner = ownerAddress;        // Set the owner of the contract on creation
     }
 
     /* Function to whitelist partner address. Can only be called by owner */
@@ -79,10 +127,10 @@ contract MyToken is owned{
        Restricted to whitelisted partners */
     function authenticate(address _to, uint256 _value, string data) {
         require(whitelist[msg.sender]);                     // Make sure the sender is whitelisted
-        _value = _value * (10 ** 18);                       // Adjust for decimals
-        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
-        require (balanceOf[_from] > _value);                // Check if the sender has enough
-        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
+        _value = _value * (10 ** decimals);                 // Adjust for decimals
+        require (_to != 0x0);                               // Prevent transfer to 0x0 address
+        require (balances[msg.sender] > _value);            // Check if the sender has enough
+        require (balances[_to] + _value > balances[_to]);   // Check for overflows
         _transfer(msg.sender, _to, _value);
         updatePartnerValuesMap(msg.sender, _value, data);
         Authenticate(msg.sender, _to, _value, msg.data);
@@ -94,19 +142,20 @@ contract MyToken is owned{
         partnerValuesMap[_sender].data = data;
     }
 
-    /* Function to update the hedgeableValuesMap. Called exclusively from the Hedgeable API */
-    function updateHedgeableValuesMap(address _sender, uint _value, string data) onlyOwner {
-        hedgeableValuesMap[_sender].value = _value;
-        hedgeableValuesMap[_sender].data = data;
-        hedgeableValuesMap[_sender].timestamp = block.timestamp + 1 days;
+    /* Function to update the hydrogenValuesMap. Called exclusively from the Hedgeable API */
+    function updateHydrogenValuesMap(address _sender, uint _value, string data) onlyOwner {
+        hydrogenValuesMap[_sender].value = _value;
+        hydrogenValuesMap[_sender].data = data;
+        hydrogenValuesMap[_sender].timestamp = block.timestamp + 1 days;
     }
 
-    /* Function called by Hedgeable API to check if the partner has validated
-       The partners value and data must match and it must be less than a day since the last authentication */
+    /* Function called by Hydrogen API to check if the partner has validated
+     * The partners value and data must match and it must be less than a day since the last authentication
+     */
     function validateAuthentication(address _sender) public constant returns (bool _isValid) {
-        if (partnerValuesMap[_sender].value == hedgeableValuesMap[_sender].value
-        && block.timestamp < hedgeableValuesMap[_sender].timestamp
-        && sha3(partnerValuesMap[_sender].data) == sha3(hedgeableValuesMap[_sender].data)){
+        if (partnerValuesMap[_sender].value == hydrogenValuesMap[_sender].value
+        && block.timestamp < hydrogenValuesMap[_sender].timestamp
+        && sha3(partnerValuesMap[_sender].data) == sha3(hydrogenValuesMap[_sender].data)){
             return true;
         }
         return false;
