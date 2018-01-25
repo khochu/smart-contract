0c41233e8f4367893eb65484ed6640b6c5e4b215
AndyHedgeable <andy@hedgeable.com>
2018-01-25 12:24:43 -0500

    More smart contract updates
    Updated all smart contracts in the repo to now match the new logic. Also updated the tests to work with the changed logic.

--- tests/tests/test_hydro.py
+++ tests/tests/test_hydro.py
@@ -20,7 +20,7 @@ def test_totalSupply(chain):
     hydro, _ = chain.provider.get_or_deploy_contract('HydroToken')
 
     supply = hydro.call().totalSupply()
-    assert supply == 1000000000000000000000000000
+    assert supply == 11111111111000000000000000000
 
 def whitelist(address, hydro, web3, chain):
     set_txn_hash = hydro.transact().whitelistAddress(address, True, 1)
@@ -41,12 +41,12 @@ def hydroPartnerMap(address, hydro, web3, chain):
     assert hydroMap[0] != 4
 
 def authenticate(address, hydro, web3, chain):
-    set_txn_hash = hydro.transact().authenticate(5, 'testdata', 1)
+    set_txn_hash = hydro.transact().authenticate(5, 1, 1)
     chain.wait.for_receipt(set_txn_hash)
 
     hydroMap = hydro.call().partnerMap(1,address)
     assert hydroMap[0] == 5
-    assert hydroMap[1] == 'testdata'
+    assert hydroMap[1] == 1
 
 def test_authenticationProcess(web3, chain):
     hydro, _ = chain.provider.get_or_deploy_contract('HydroToken')
@@ -59,11 +59,11 @@ def test_authenticationProcess(web3, chain):
 
     authenticate(address, hydro, web3, chain)
 
-    authenticated = hydro.call().validateAuthentication(address, 'testdata', 1)
+    authenticated = hydro.call().validateAuthentication(address, 1, 1)
     assert authenticated == True
 
-    authenticated = hydro.call().validateAuthentication(address, 'wrongData', 1)
+    authenticated = hydro.call().validateAuthentication(address, 2, 1)
     assert authenticated == False
 
-    authenticated = hydro.call().validateAuthentication(address, 'testdata', 2)
+    authenticated = hydro.call().validateAuthentication(address, 1, 2)
     assert authenticated == False
